3568d60c3b16dd048517f8581fc9b257e138e661
ChillinwithCloud <chillinwithcloud@gmail.com>
2023-12-21 18:28:18 +0530

    second commit

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ let buttons = document.querySelectorAll('button');
 let number = "";
 
 buttons.forEach((button) => {
-    button.addEventListener('click' , a =>{
-        if(a.target.innerHTML == '='){
+    button.addEventListener('click' , e =>{
+        if(e.target.innerHTML == '='){
             number = eval(number);
             output.value = number;
         }
-        else if(a.target.innerHTML == 'AC'){
+        else if(e.target.innerHTML == 'AC'){
             number = "";
             output.value = number;
         }
-        else if(a.target.innerHTML == '⌫'){
+        else if(e.target.innerHTML == '⌫'){
             number = number.slice(0, number.length-1);
             output.value = number;
         }
         else{
-            number += a.target.innerHTML;
+            number += e.target.innerHTML;
             output.value = number;
         }
 
